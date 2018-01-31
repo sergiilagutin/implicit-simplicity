647dfdd11b873692d81404ed89e8e2ef211e4cb2
sergey-lagutin <sergey.v.lagutin@gmail.com>
2018-01-31 16:32:45 +0100

    Remove comments.

--- src/main/scala/com/anadea/conversions/ImplicitConversion.scala
+++ src/main/scala/com/anadea/conversions/ImplicitConversion.scala
@@ -11,18 +11,3 @@ object ImplicitConversion extends App {
 
   call(1)
 }
-
-/**
-    package com.anadea.conversions {
-      import scala.language.implicitConversions;
-      object ImplicitConversion extends AnyRef with App {
-        def <init>(): com.anadea.conversions.ImplicitConversion.type = {
-          ImplicitConversion.super.<init>();
-          ()
-        };
-        def call(str: String): Unit = scala.Predef.println(str);
-        implicit def intToString(i: Int): String = i.toString();
-        ImplicitConversion.this.call(ImplicitConversion.this.intToString(1))
-      }
-    }
-  */
